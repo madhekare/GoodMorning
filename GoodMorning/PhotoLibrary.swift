e9c8b2f509496973e10477bc86d235908cf0d1b9
Bohui (Dennis) Moon <bohuim@google.com>
2018-11-03 17:53:28 -0700

    Basic photo loading

--- GoodMorning/PhotoLibrary.swift
+++ GoodMorning/PhotoLibrary.swift
@@ -9,48 +9,54 @@
 import UIKit
 import Photos
 
-class PhotoLibrary {
+class PhotoLibrary
+{
+    var images: [UIImage] = []
     
-    fileprivate var imgManager: PHImageManager
-    fileprivate var requestOptions: PHImageRequestOptions
-    fileprivate var fetchOptions: PHFetchOptions
-    fileprivate var fetchResult: PHFetchResult<PHAsset>
-    
-    init () {
-        imgManager = PHImageManager.default()
-        requestOptions = PHImageRequestOptions()
-        requestOptions.isSynchronous = true
-        fetchOptions = PHFetchOptions()
+    // Fetch images from the system photo library
+    func fetchLibrary() {
+        let fetchOptions = PHFetchOptions()
         fetchOptions.sortDescriptors = [NSSortDescriptor(key:"creationDate", ascending: true)]
-        fetchResult = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: fetchOptions)
-    }
-    
-    var count: Int {
-        return fetchResult.count
-    }
-    
-    func setPhoto(at index: Int, completion block: @escaping (UIImage?)->()) {
+        let fetchResult = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: fetchOptions)
         
-        if index < fetchResult.count  {
-            imgManager.requestImage(for: fetchResult.object(at: index) as PHAsset, targetSize: UIScreen.main.bounds.size, contentMode: PHImageContentMode.aspectFill, options: requestOptions) { (image, _) in
-                block(image)
-            }
-        } else {
-            block(nil)
-        }
-    }
-    
-    func getAllPhotos() -> [UIImage] {
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isSynchronous = true
         
-        var resultArray = [UIImage]()
-        for index in 0..<fetchResult.count {
-            imgManager.requestImage(for: fetchResult.object(at: index) as PHAsset, targetSize: UIScreen.main.bounds.size, contentMode: PHImageContentMode.aspectFill, options: requestOptions) { (image, _) in
-                
-                if let image = image {
-                    resultArray.append(image)
-                }
+        fetchResult.enumerateObjects { (asset, index, stop) in
+            // Convert each PHAsset into an UIImage
+            PHImageManager.default().requestImage(for: asset,
+                                                  targetSize: CGSize(width: 100, height: 100),
+                                                  contentMode: .aspectFit,
+                                                  options: requestOptions)
+            { [weak self] (image: UIImage?, info) in
+                // if image isn't nil, let's add it
+                if let image = image { self?.images.append(image) }
             }
         }
-        return resultArray
     }
+    
+//    func setPhoto(at index: Int, completion block: @escaping (UIImage?)->()) {
+//        
+//        if index < fetchResult.count  {
+//            imgManager.requestImage(for: fetchResult.object(at: index) as PHAsset, targetSize: UIScreen.main.bounds.size, contentMode: PHImageContentMode.aspectFill, options: requestOptions) { (image, _) in
+//                block(image)
+//            }
+//        } else {
+//            block(nil)
+//        }
+//    }
+//    
+//    func getAllPhotos() -> [UIImage] {
+//        
+//        var resultArray = [UIImage]()
+//        for index in 0..<fetchResult.count {
+//            imgManager.requestImage(for: fetchResult.object(at: index) as PHAsset, targetSize: UIScreen.main.bounds.size, contentMode: PHImageContentMode.aspectFill, options: requestOptions) { (image, _) in
+//                
+//                if let image = image {
+//                    resultArray.append(image)
+//                }
+//            }
+//        }
+//        return resultArray
+//    }
 }
